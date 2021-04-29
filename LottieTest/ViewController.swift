0229451a06598980a68476d2c4a3b29b73888456
Suhaas <suhaask.1999@gmail.com>
2021-04-29 17:44:55 +0530

    Initial Commit

---
+++ LottieTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieTest
+//
+//  Created by Suhaas Kumbhajadala on 29/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
